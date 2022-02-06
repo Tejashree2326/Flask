bc0e8bc74479137d576c1f70a930a7ac9a528e7b
Tejashree Patil <tejashreepatil51569@gmail.com>
2022-02-06 17:42:59 +0530

    final changes

--- app.py
+++ app.py
@@ -4,15 +4,18 @@ from flask_cors import CORS
 from flask_jwt_extended import create_access_token, JWTManager, jwt_required, get_jwt_identity
 from bson.objectid import ObjectId
 from bson import json_util
-from datetime import datetime
+from datetime import datetime, timedelta
 
 app = Flask(__name__)
 
 app.config["JWT_SECRET_KEY"] = "super-secret"  # Change this "super secret" with something else!
+app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(days=30)
 jwt = JWTManager(app)
 
 CORS(app)
 
+#Used to perform connection and operation with mongodb
+#https://www.mongodb.com/languages/python#:~:text=The%20first%20step%20to%20connect,text%20editor%20like%20Textpad%2FNotepad.&text=Use%20the%20connection_string%20to%20create,get%20the%20MongoDB%20database%20connection.
 client = pymongo.MongoClient("mongodb://c21123464:Yutika2326@csmongo.cs.cf.ac.uk:27017/c21123464?authSource=c21123464")
 db = client.c21123464
 
@@ -32,12 +35,37 @@ def post_blog():
     db.blogs.insert_one({'title': request.form.get('title'), 'description': request.form.get('description'),'email': user['email'], 'firstName': user['firstName'], 'comment': [], 'average_rating': 0, 'image': request.form.get('image'), 'createdAt': date})
 
     return "Success"
+    
+@app.route('/post-comment', methods=['POST'])
+@jwt_required()
+def post_comment():
+    id = get_jwt_identity()
+    user = db.blogusers.find_one({'_id': ObjectId(id)})
+
+    date = datetime.now()
+
+    db.blogs.update_one({'_id': ObjectId(request.form.get('id'))},{ '$push' : {'comment': {'firstName': request.form.get('firstName'),'comment': request.form.get('comment'), 'createdAt': date}}})
+
+    return "Success"
+
+@app.route('/post-rating', methods=['POST'])
+@jwt_required()
+def post_rating():
+    id = get_jwt_identity()
+    user = db.blogusers.find_one({'_id': ObjectId(id)})
+
+    date = datetime.now()
+
+
+    rating = db.blogs.find_one({'_id': ObjectId(request.form.get('id'))})['average_rating']
+    avgrate= (rating + int(request.form.get('rating')))/2
+    db.blogs.update_one({'_id': ObjectId(request.form.get('id'))},{ '$set' : {'average_rating': avgrate}})
+    return "Success"
 
 @app.route('/get-blog', methods=['GET'])
 def get_blog():
     blogs = db.blogs.find({})
     blogslist = list(blogs)
-    print(blogslist)
     return json_util.dumps(blogslist)
 
 @app.route('/register', methods=['POST'])
@@ -52,7 +80,6 @@ def sign_up():
 def login():
         user = db.blogusers.find_one({'email': request.form.get('email')})
         firstName = db.blogusers.find_one({'email': request.form.get('email')})['firstName']
-        print(firstName)
         if(user):
             if(user['password'] == request.form.get('password')):
                 token = create_access_token(str(db.blogusers.find_one({'email': request.form.get('email')})['_id']))
